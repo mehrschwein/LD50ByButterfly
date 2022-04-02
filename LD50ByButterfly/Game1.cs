9031035b0ff75e2a862c165e5db56ce36c60da05
butterfly <damian@stobbenet.de>
2022-04-02 21:13:44 +0200

    [LD50] working window rezising, working clock, initial money and sleep timer

--- LD50ByButterfly/Game1.cs
+++ LD50ByButterfly/Game1.cs
@@ -11,6 +11,7 @@ namespace LD50ByButterfly
         private SpriteBatch _spriteBatch;
 
         public Rectangle screenDimensions = new Rectangle(0, 0, 512, 320);
+        public float windowScale = 1.0f;
 
         enum GameState
         {
@@ -26,18 +27,48 @@ namespace LD50ByButterfly
             DoubleSize,
             Fullscreen
         }
+
+        private SpriteFont _font;
         #endregion
 
         #region individual assets
+        private WallClock _wallClock = new WallClock();
+        double runStartTime = 0;
+        double lastClockTick = 0;
+
+        int sleepTimer = 100;
+        double sleepSpeedTick = 0;
+        double sleepyness = 500;
+
+        int money = 0;
+        int allTimeMoney = 0;
+        double lastMoneyTick = 0;
 
         private Texture2D _texBackground;
+        private Texture2D _texHourArrow;
+        private Texture2D _texMinuteArrow;
+        private Texture2D _texForeGround;
+        private Texture2D _texMug;
+        private Texture2D _texFlamingo;
+
+        private Texture2D _texMugBar;
+        private Rectangle _rectMugBar;
+        private Rectangle _rectMugDefault;
+        private Rectangle _rectMugCoffeemachine;
+        private Rectangle _rectCoffeemachine;
 
         #endregion
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);SwitchToScreenSize(WindowSize.SizeByPixel);
-            //Window.AllowUserResizing = true;
+
+            Window.Title = "desk flamingo";
+            Window.AllowUserResizing = false;
+
+            //SwitchToScreenSize(WindowSize.DoubleSize);
+            windowScale = 2.0f;
+            ReloadScreensize();
 
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
@@ -48,22 +79,35 @@ namespace LD50ByButterfly
             switch (changeSize)
             {
                 case WindowSize.SizeByPixel:
-                    _graphics.PreferredBackBufferWidth = screenDimensions.Width;
-                    _graphics.PreferredBackBufferHeight = screenDimensions.Height;
+                    _graphics.PreferredBackBufferWidth = 512;
+                    _graphics.PreferredBackBufferHeight = 320;
                     _graphics.ApplyChanges();
                     break;
                 case WindowSize.DoubleSize:
-                    _graphics.PreferredBackBufferWidth = screenDimensions.Width*2;
-                    _graphics.PreferredBackBufferHeight = screenDimensions.Height*2;
+                    _graphics.PreferredBackBufferWidth = 512 * 2;
+                    _graphics.PreferredBackBufferHeight = 320 * 2;
                     _graphics.ApplyChanges();
                     break;
                 case WindowSize.Fullscreen:
-                    _graphics.PreferredBackBufferWidth = screenDimensions.Width * 2;
-                    _graphics.PreferredBackBufferHeight = screenDimensions.Height * 2;
+                    _graphics.PreferredBackBufferWidth = 512 * 2;
+                    _graphics.PreferredBackBufferHeight = 320 * 2;
                     _graphics.ApplyChanges();
                     break;
             }
+            screenDimensions.Width = _graphics.PreferredBackBufferWidth;
+            screenDimensions.Height = _graphics.PreferredBackBufferHeight;
         }
+
+        void ReloadScreensize()
+        {
+            _graphics.PreferredBackBufferWidth = (int)(512 * windowScale);
+            _graphics.PreferredBackBufferHeight = (int)(320 * windowScale);
+            _graphics.ApplyChanges();
+
+            screenDimensions.Width = _graphics.PreferredBackBufferWidth;
+            screenDimensions.Height = _graphics.PreferredBackBufferHeight;
+        }
+            
 
         protected override void Initialize()
         {
@@ -77,39 +121,46 @@ namespace LD50ByButterfly
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            _font = Content.Load<SpriteFont>("Pixeled");
+
             _texBackground = Content.Load<Texture2D>("Background");
+            _texHourArrow = Content.Load<Texture2D>("HourArrow");
+            _texMinuteArrow = Content.Load<Texture2D>("MinuteArrow");
+            _texFlamingo = Content.Load<Texture2D>("FlamingoWorker");
+            _texForeGround = Content.Load<Texture2D>("foreground");
+            _texMug = Content.Load<Texture2D>("Mug");
         }
         #endregion
 
         #region update logic block
         protected override void Update(GameTime gameTime)
         {
-            /*if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();*/
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            if (Keyboard.GetState().IsKeyDown(Keys.P))
             {
-                SwitchToScreenSize(WindowSize.SizeByPixel);
+                windowScale += 0.02f;
+                ReloadScreensize();
             }
-            else if (Keyboard.GetState().IsKeyDown(Keys.D))
+            else if (Keyboard.GetState().IsKeyDown(Keys.OemMinus))
             {
-                SwitchToScreenSize(WindowSize.DoubleSize);
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.F))
-            {
-                SwitchToScreenSize(WindowSize.Fullscreen);
+                windowScale -= 0.02f;
+                ReloadScreensize();
             }
 
             switch (_gameState)
             {
                 case GameState.StartMenu:
-                    UpdateStartMenu();
+                    UpdateStartMenu(gameTime);
                     break;
                 case GameState.OfficeView:
-                    UpdateOfficeView();
+                    UpdateOfficeView(gameTime);
                     break;
                 case GameState.EndScreen:
-                    UpdateEndScreen();
+                    UpdateEndScreen(gameTime);
                     break;
             }
 
@@ -118,21 +169,49 @@ namespace LD50ByButterfly
 
 
         //update logic for Main Menu
-        private void UpdateStartMenu()
+        private void UpdateStartMenu(GameTime gameTime)
         {
 
+            if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+            {
+                _gameState = GameState.OfficeView;
+            }
         }
 
         //update logic for Game/Office
-        private void UpdateOfficeView()
+        private void UpdateOfficeView(GameTime gameTime)
         {
+            //_wallClock.AddTime();
+            if ((gameTime.TotalGameTime.TotalMilliseconds - runStartTime) > (lastClockTick + 300))
+            {
+                _wallClock.AddTime();
+                sleepTimer--;
+                lastClockTick = gameTime.TotalGameTime.TotalMilliseconds;
+            }
 
+            if ((gameTime.TotalGameTime.TotalMilliseconds - runStartTime) > (sleepSpeedTick + sleepyness))
+            {
+                sleepTimer--;
+                sleepyness -= 3;
+                sleepSpeedTick = gameTime.TotalGameTime.TotalMilliseconds;
+            }
+
+            if ((gameTime.TotalGameTime.TotalMilliseconds - runStartTime) > (lastMoneyTick + 1000))
+            {
+                money++;
+                allTimeMoney++;
+                lastMoneyTick = gameTime.TotalGameTime.TotalMilliseconds;
+            }
         }
 
         //update logic for End Screen
-        private void UpdateEndScreen()
+        private void UpdateEndScreen(GameTime gameTime)
         {
 
+            if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+            {
+                _gameState = GameState.OfficeView;
+            }
         }
         #endregion
 
@@ -171,6 +250,19 @@ namespace LD50ByButterfly
         private void DrawOfficeView()
         {
             _spriteBatch.Draw(_texBackground, screenDimensions, Color.White);
+            //_spriteBatch.Draw(_texHourArrow, new Rectangle((int)(392*windowScale), (int)(84 * windowScale), (int)(50 * windowScale), (int)(50 * windowScale)), Color.White);
+            //_spriteBatch.Draw(_texMinuteArrow, new Rectangle((int)(392 * windowScale), (int)(84 * windowScale), (int)(50 * windowScale), (int)(50 * windowScale)), Color.White);
+            Vector2 clockPos = new Vector2((int)((392 + 25) * windowScale), (int)((84+25) * windowScale));
+            Vector2 clockOrigin = new Vector2((int)(25), (int)(25));
+            _spriteBatch.Draw(_texHourArrow, clockPos, null, Color.White, _wallClock.GetHourRotation(), clockOrigin, windowScale, SpriteEffects.None, 1);
+            _spriteBatch.Draw(_texMinuteArrow, clockPos, null, Color.White, _wallClock.GetMinuteRotation(), clockOrigin, windowScale, SpriteEffects.None, 1);
+            _spriteBatch.Draw(_texFlamingo, screenDimensions, Color.White);
+            _spriteBatch.Draw(_texForeGround, screenDimensions, Color.White);
+            _spriteBatch.Draw(_texMug, new Rectangle((int)(300 * windowScale), (int)(206 * windowScale), (int)(18 * windowScale), (int)(18 * windowScale)), Color.White);
+
+            _spriteBatch.DrawString(_font, "$ " + money, new Vector2(20, 20), Color.Black);
+            //_spriteBatch.DrawString(_font, "Sleep Timer:  " + sleepTimer, new Vector2(20, 50), Color.Black);
+            //_spriteBatch.DrawString(_font, "Sleepyness:  " + sleepyness, new Vector2(20, 80), Color.Black);
         }
 
         //draw logic for End Screen
---
+++ LD50ByButterfly/WallClock.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace LD50ByButterfly
+{
+    class WallClock
+    {
+        public int Hours = 0;
+        public int Minutes = 0;
+        public int HighscoreHours = 0;
+        public int HighscoreMinutes = 0;
+
+
+        public string GetHighscore()
+        {
+            return HighscoreHours + ":" + HighscoreMinutes;
+        }
+
+        public string GetScore()
+        {
+            return Hours + ":" + Minutes;
+        }
+
+        public float GetHourRotation()
+        {
+            return MathHelper.ToRadians( Hours * 30);
+        }
+
+        public float GetMinuteRotation()
+        {
+            return MathHelper.ToRadians(Minutes * 6);
+        }
+
+        public void AddTime()
+        {
+            Minutes += 5;
+            if (Minutes >= 60)
+            {
+                Hours++;
+                Minutes = 0;
+            }
+        }
+        public void ResetTime()
+        {
+            Hours = 0;
+            Minutes = 0;
+        }
+
+        public bool HigherThanHighscore()
+        {
+            if (Hours > HighscoreHours)
+            {
+                return true;
+            }
+            else if (Hours >= HighscoreHours && Minutes > HighscoreMinutes)
+            {
+                return true;
+            }
+            
+            return false;
+        }
+
+    }
+}
